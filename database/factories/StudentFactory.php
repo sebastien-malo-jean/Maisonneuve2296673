64a020bf2166f3368e21a6b7a96908e08b9034b6
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-02-27 03:52:37 -0500

    Ajout d'une factory pour le modèle Student

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\City;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'address' => $this->faker->address(),
+            'phone' => $this->faker->phoneNumber(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'dateOfBirth' => $this->faker->date(),
+            'city_id' => City::inRandomOrder()->first()->id,
+        ];
+    }
+}
