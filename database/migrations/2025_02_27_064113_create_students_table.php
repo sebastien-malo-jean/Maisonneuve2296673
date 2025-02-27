7dddc312d6bb542d4154bd363cc058932c8bf102
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-02-27 02:31:18 -0500

    Ajout des colonnes pour le modèle Student et définition de la clé étrangère vers le modèle City

--- database/migrations/2025_02_27_064113_create_students_table.php
+++ database/migrations/2025_02_27_064113_create_students_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('email')->unique();
+            $table->date('dateOfBirth');
+            $table->unsignedBigInteger('city_id');
+            $table->foreign('city_id')->references('id')->on('cities');
             $table->timestamps();
         });
     }
