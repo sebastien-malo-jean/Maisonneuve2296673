3b6fa736663fc46834e1a5095413472f996c6553
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-03-01 03:35:37 -0500

    Mise à jour du composant de carte étudiant pour afficher les informations du student, y compris le nom, l'adresse, le téléphone, l'email, la date de naissance et la ville.

--- resources/views/layouts/components/studentCard.blade.php
+++ resources/views/layouts/components/studentCard.blade.php
@@ -1,15 +1,19 @@
 <article class="card">
     <section class="card-header">
-        <h3 class="card-title"></h3>
+        <h3 class="card-title">{{ $student->name }}</h3>
     </section>
     <section class="card-body">
-        <p class="card-text"></p>
+        <ul class="list-unstyled">
+            <li><strong>Adresse : </strong>{{ $student->address }}</li>
+            <li><strong>Téléphone : </strong>{{ $student->phone }}</li>
+            <li><strong>email : </strong>{{ $student->email }}</li>
+            <li><strong>Aniversaire : </strong>{{ $student->dateOfBirth }}</li>
+            <li><strong>Ville : </strong>{{ $student->city_id }}</li>
+        </ul>
     </section>
     <footer class="card-footer">
         <div class="d-flex justify-content-between">
-            <a href="#" class="btn btn-primary">Voir</a>
-            <a href="#" class="btn btn-warning">Modifier</a>
-            <a href="#" class="btn btn-danger">Supprimer</a>
+            <a href="{{ route("student.show", $student->id) }}" class="btn btn-primary">Voir</a>
         </div>
     </footer>
 </article>
