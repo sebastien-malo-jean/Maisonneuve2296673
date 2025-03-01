d6628e6d058aa49ee39c267208d9b907a1611d76
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-03-01 03:52:39 -0500

    Ajout d'une feuille de style CSS de base pour le projet

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,12 @@
     <meta name="description" content="">
     <meta charset='UTF-8'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <!-- bootstrap css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- css de base -->
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
     <!-- icônes Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 
