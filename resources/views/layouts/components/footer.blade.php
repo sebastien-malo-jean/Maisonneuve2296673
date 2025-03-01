f26711d8cdf7c06dacf75f319a18890fbd99aaf1
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-03-01 03:07:46 -0500

    Mise à jour du pied de page avec des icônes de réseaux sociaux et des informations de copyright

--- resources/views/layouts/components/footer.blade.php
+++ resources/views/layouts/components/footer.blade.php
@@ -1,22 +1,24 @@
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <h3>À propos</h3>
-                <p>Le site web des étudiants du Collège Maisonneuve.</p>
-            </div>
-            <div class="col-md-4">
-                <h3>Liens rapides</h3>
-                <ul>
-                    <li><a href="{{ route('student.index') }}">Étudiants</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </div>
-            <div class="col-md-4">
-                <h3>Contact</h3>
-                <p>3800 R. Sherbrooke E, Montréal, QC H1X 2A2</p>
-            </div>
-        </div>
-    </div>
-</footer>
+<div class="col-md-4 d-flex align-items-center">
+    <span class="mb-3 mb-md-0 text-body-secondary">&copy; {{ date('Y') }} {{ config('app.name') }}</span>
+</div>
+
+<ul class="nav col-md-4 justify-content-end list-unstyled d-flex align-items-center">
+    <li class="ms-3">
+        <a class="text-body-secondary" href="#">
+            <i class="bi bi-twitter" style="font-size: 24px;">
+            </i>
+        </a>
+    </li>
+    <li class="ms-3">
+        <a class="text-body-secondary" href="#">
+            <i class="bi bi-facebook" style="font-size: 24px;">
+            </i>
+        </a>
+    </li>
+    <li class="ms-3">
+        <a class="text-body-secondary" href="#">
+            <i class="bi bi-instagram" style="font-size: 24px;">
+            </i>
+        </a>
+    </li>
+</ul>
