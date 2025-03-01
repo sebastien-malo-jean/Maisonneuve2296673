3596dcb51bf1b81e500dcdf1db5f30ce6c8027e9
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-03-01 05:12:26 -0500

    Amélioration de la présentation des informations dans la carte étudiant

--- resources/views/layouts/components/studentCard.blade.php
+++ resources/views/layouts/components/studentCard.blade.php
@@ -4,16 +4,16 @@
     </section>
     <section class="card-body">
         <ul class="list-unstyled">
-            <li><strong>Adresse : </strong>{{ $student->address }}</li>
-            <li><strong>Téléphone : </strong>{{ $student->phone }}</li>
-            <li><strong>email : </strong>{{ $student->email }}</li>
-            <li><strong>Aniversaire : </strong>{{ $student->dateOfBirth }}</li>
-            <li><strong>Ville : </strong>{{ $student->city_id }}</li>
+            <li><strong>Adresse</strong><br>{{ $student->address }}</li>
+            <li><strong>Téléphone</strong><br>{{ $student->phone }}</li>
+            <li><strong>email</strong><br>{{ $student->email }}</li>
+            <li><strong>Aniversaire</strong><br>{{ $student->dateOfBirth }}</li>
+            <li><strong>Ville</strong><br>{{ $student->city_id }}</li>
         </ul>
     </section>
     <footer class="card-footer">
-        <div class="d-flex justify-content-between">
-            <a href="{{ route("student.show", $student->id) }}" class="btn btn-primary">Voir</a>
+        <div class="text-center">
+            <a href="#" class="btn btn-primary">Voir l'étudiant</a>
         </div>
     </footer>
 </article>
