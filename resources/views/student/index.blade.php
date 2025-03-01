096e694b968f7fce30fe5ff1d3eb64798a41852a
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-03-01 05:13:11 -0500

    Ajout d'un en-tête descriptif et d'un composant de filtre pour la liste des étudiants

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -2,6 +2,21 @@
 @section('title', 'Students')
 @section('content')
 
-<h1>Liste des étudiants</h1>
+<header class="mb-3">
+    <h1>Liste des étudiants</h1>
+    <h2>Voici la liste de tous les Étudiants disponibles sur la platforme.</h2>
+</header>
 
+<div class="row">
+    <div class="col-md-2">
+        @include('layouts.components.filter')
+    </div>
+    <div class="col-md-10 row">
+        @foreach($students as $student)
+        <div class="col-md-4 mb-3">
+            @include('layouts.components.studentCard')
+        </div>
+        @endforeach
+    </div>
+</div>
 @endsection
