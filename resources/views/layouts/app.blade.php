a46decda0154c66ae06fa6bf8e000aae64e55899
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-03-01 03:08:02 -0500

    Ajout de Bootstrap 5 pour le style et la mise en page, et amélioration de la structure du layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,21 +6,35 @@
     <meta name="description" content="">
     <meta charset='UTF-8'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- icônes Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+
+
     <title>@yield('title')</title>
+
 </head>
 
-<body>
-    <header>
+<body class="d-flex flex-column vh-100">
+    <header class="p-3">
         @include('layouts.components.navbar')
     </header>
 
-    <main>
+    <main class="flex-shrink-0 p-3">
         @yield('content')
     </main>
 
-    <footer>
+    <footer
+        class="footer mt-auto d-flex flex-wrap justify-content-between align-items-center py-3 px-3 my-4 border-top">
         @include('layouts.components.footer')
     </footer>
+
+    <!-- Bootstrap 5 Bundle (inclut Popper.js) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
