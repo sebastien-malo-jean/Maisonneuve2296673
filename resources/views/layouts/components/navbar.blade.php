ceff69ee541cfc44f1028705ef06a9ab0dfe342f
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-03-01 01:23:41 -0500

    Mise à jour de la barre de navigation avec un nouveau design et des liens améliorés

--- resources/views/layouts/components/navbar.blade.php
+++ resources/views/layouts/components/navbar.blade.php
@@ -1,7 +1,28 @@
-<nav>
-    <ul>
-        <li><a href="{{ route('student.index') }}">Étudiants</a></li>
-        <li><a href="#">À propos</a></li>
-        <li><a href="#">Contact</a></li>
-    </ul>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="/">Maisonneuve Connect</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="/">Accueil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/students">Étudiants</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-bs-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    Compte
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Connexion</a>
+                    <a class="dropdown-item" href="#">Création de compte</a>
+                    <a class="dropdown-item" href="#">Déconnexion</a>
+                </div>
+            </li>
+        </ul>
+    </div>
 </nav>
