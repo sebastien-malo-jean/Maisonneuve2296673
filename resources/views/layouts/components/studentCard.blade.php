dfb26bec94433d3328ac06de93f8737e48ac4f12
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-03-01 03:20:39 -0500

    Ajout d'un composant de carte étudiant pour une meilleure présentation des informations

---
+++ resources/views/layouts/components/studentCard.blade.php
@@ -0,0 +1,15 @@
+<article class="card">
+    <section class="card-header">
+        <h3 class="card-title"></h3>
+    </section>
+    <section class="card-body">
+        <p class="card-text"></p>
+    </section>
+    <footer class="card-footer">
+        <div class="d-flex justify-content-between">
+            <a href="#" class="btn btn-primary">Voir</a>
+            <a href="#" class="btn btn-warning">Modifier</a>
+            <a href="#" class="btn btn-danger">Supprimer</a>
+        </div>
+    </footer>
+</article>
