661554d7679476b54b5661ccd975247f05d71b3c
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-04-08 16:07:51 -0400

    Ajout de la méthode store dans UserController pour gérer la création d'utilisateur avec validation des données.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -28,7 +29,18 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|max:20'
+        ]);
+
+        $user = new User;
+        $user->fill($request->all());
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return redirect(route('user.index'))->withSuccess('User created successfully!');
     }
 
     /**
